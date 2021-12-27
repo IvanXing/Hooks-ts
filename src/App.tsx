83523c806ba1a9b2320c1247b499120da29ef33f
xingyifan <xingyifan10@qq.com>
2021-12-27 21:20:49 +0800

    feat: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Hello from './components/Hello';
+import LikeButton from './components/LikeButton';
 
 function App() {
   return (
     <div className="App">
      <Hello message="helloWorld" />
+     <LikeButton />
     </div>
   );
 }
