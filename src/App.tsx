ba5b187ab6915aadcc38905a52908db1db1e391e
xingyifan <xingyifan10@qq.com>
2021-12-28 13:29:39 +0800

    feat: 抽离一个自定义hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import Hello from './components/Hello';
 import LikeButton from './components/LikeButton';
 import MouseTracker from './components/MouseTracker';
+import useMousePositon from './hooks/useMousePosition';
 
 function App() {
   const [showMouse, setShowMouse] = useState(true);
   const [showLike, setShowLike] = useState(true);
+  const positions = useMousePositon();
   return (
     <div className="App">
       <Hello message="helloWorld" />
@@ -22,7 +24,7 @@ function App() {
           showLike && <LikeButton />
         }
       </div>
-
+      <h3>X: {positions.x}, Y : {positions.y}</h3>
     </div>
   );
 }
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import useMousePositon from '../hooks/useMousePosition';
 
 const LikeButton: React.FC = () => {
   const [like, setLike] = useState(0);
   const [on, setOn] = useState(true);
 
+  const positions = useMousePositon();
+
   useEffect(() => {
     document.title = `点击了${like}次`
   }, [like])
-  
+
   return <>
+    <h2>X: {positions.x}, Y : {positions.y}</h2>
     <button onClick={() => { setLike(like + 1) }}>{like}</button>
     <button onClick={() => { setOn(!on) }}>{on ? 'ON' : 'OFF'}</button>
   </>
