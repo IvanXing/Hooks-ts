009d68564c550451f37a298f2a2257f3e3e99312
xingyifan <xingyifan10@qq.com>
2021-12-28 11:57:00 +0800

    feat: 消除副作用的时机

---
+++ src/components/MouseTracker.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const MouseTracker: React.FC = () => {
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log('add effect', position.x)
+    const updateMouse = (e: MouseEvent) => {
+      setPosition({...position, x: e.clientX, y: e.clientY})
+    }
+    document.addEventListener('click', updateMouse);
+    return () => {
+      console.log('remove effect', position.x)
+      document.removeEventListener('click', updateMouse)
+    }
+  })
+  console.log('first render', position.x)
+  return (
+    <p>X: {position.x}, Y: {position.y}</p>
+  )
+}
+
+export default MouseTracker;
